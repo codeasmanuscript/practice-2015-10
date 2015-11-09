d1c48d045b21a9285192830bfeb54f98d27631d6
Melissa J Polonenko <mpolonenko@gmail.com>
2015-11-09 17:23:11 -0500

    added challenge 2

--- melissa-polonenko/wrangling/challenge.R
+++ melissa-polonenko/wrangling/challenge.R
@@ -17,3 +17,14 @@ ds %>%
   tbl_df()
   
 # Challenge 2
+ds %>%
+  filter(Catholic<60) %>%
+  mutate(Fertile=ifelse(Fertility>50,'Fertile','Infertile')) %>%
+  select(-rowname,-Catholic,-Fertility) %>%
+  gather(Variable,Value,-Fertile) %>% 
+  group_by(Fertile,Variable) %>%
+  summarise(meanSD=paste0(mean(Value) %>% round(2),'(',sd(Value) %>% round(2),' SD)')) %>%
+  spread(Fertile,meanSD) %>%
+  tbl_df()
+
+# Challenge 3
